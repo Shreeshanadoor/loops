1fa78528b5eb7fa550b2bfa2bc6dfe733cf69fd0
Shreeshanadoor <Shreeshanadoor>
2021-08-23 16:00:05 +0530

    fact

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int fact = 1;
+            Console.WriteLine("Enter a no");
+            int n = Convert.ToInt32(Console.ReadLine());
+            for(int i=1;i<=n;i++)
+            {
+                fact = fact * i;
+            }
+            Console.WriteLine("Factorial of" +"n"+ "is:" + fact);
+        }
+    }
+}
