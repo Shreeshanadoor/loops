7f68c195181da16e550d0f8df6910e14e07c0bee
Shreeshanadoor <Shreeshanadoor>
2021-08-23 16:11:52 +0530

    fibo

---
+++ ConsoleApp1/fibo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace fibo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1 = 0, n2 = 1, n3, n;
+            Console.WriteLine("Enter the number");
+            n = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(n1 + " " + n2 + " ");
+            for(int i=2;i<n;i++)
+            {
+                n3 = n1 + n2;
+                Console.WriteLine(n3 + " ");
+                n1 = n2;
+                n2 = n3;
+            }
+
+        }
+    }
+}
